b3d0824143a9e01e4d1dce7c8444ac8e3e10b255
Dalen Chan <dalen.chan@hotmail.com>
2021-07-09 19:23:00 +0100

    JS added to update clock every .5 sec

---
+++ script.js
@@ -0,0 +1,41 @@
+class DigitalClock {
+    constructor(element) {
+        this.element = element;
+    }
+
+    start() {
+        this.update();
+
+        setInterval(() => {
+            this.update();
+        }, 500);
+    }
+
+    // Method to update HTML 
+    update () {
+        const parts = this.getTimeParts();
+        // Add in '0' character 
+        const minuteFormatted = parts.minute.toString().padStart(2, "0");
+        const timeFormatted = `${parts.hour}:${minuteFormatted}`;
+        // If isAm = true then print AM if not print PM 
+        const amPm = parts.isAM ? "AM" : "PM";
+
+        this.element.querySelector('.clock-time').textContent = timeFormatted;
+        this.element.querySelector('.clock-ampm').textContent = amPm;
+    }
+
+    // Method to get the time 
+    getTimeParts () {
+        const now = new Date(); 
+
+        return {
+            hour: now.getHours() % 12 || 12,
+            minute: now.getMinutes(),
+            isAM: now.getHours() < 12
+        };
+    }
+}
+const clock = document.querySelector('.clock');
+const clockObject = new DigitalClock(clock);
+
+clockObject.start();
